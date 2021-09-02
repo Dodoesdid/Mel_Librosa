27a9d8b4f786bb15b49d3d4c31194f7056a964f9
Jonathan Tang <jonathantang2015@gmail.com>
2021-09-02 16:15:33 +0800

    used the quarter method to reduce noise

---
+++ Done.py
@@ -0,0 +1,59 @@
+from re import T
+
+from numpy.fft import irfft
+import Mel as m
+import numpy as np
+import scipy.signal
+import soundfile as sf
+import matplotlib.pyplot as plt
+
+WINDOW_OFFSET = 0.0001
+FRAME_SIZE = 1024
+HOP_SIZE = 512
+
+""" FFT test """
+# test data
+y, sr = sf.read("audio/mixed.wav")
+
+plt.plot(y)
+plt.title("Original Audio")
+plt.show()
+
+# test numpy fft on audio
+f_fft = np.fft.rfft(y)
+y_fft = np.fft.irfft(f_fft)
+
+plt.plot(y_fft)
+plt.title("Converted Audio")
+plt.show()
+
+sf.write("audio_generated/mixed_fft.wav", y_fft, sr)
+
+""" Window test """
+# Generate test data
+x = np.linspace(0, 10 * np.pi, 1000)
+sine = np.sin(x)
+
+plt.plot(sine)
+plt.title("Original Data")
+plt.show()
+
+# Generate Window
+sine_window = scipy.signal.get_window("hann", 1000, True)
+
+# Apply window
+sine_a = sine * sine_window
+
+# Apply foward n reverse fft
+sine_a = np.fft.irfft(np.fft.rfft(sine_a))
+
+# Remove window
+sine_a = sine_a / sine_window
+
+plt.plot(sine_a)
+plt.title("After Procedure")
+plt.show()
+
+plt.plot(sine - sine_a)
+plt.title("Difference")
+plt.show()
--- Mel_np.py
+++ Mel_np.py
@@ -1,35 +1,73 @@
-import soundfile as sf
+from Reference.mel_featues import frame
+import vggish_mel_features as m
 import numpy as np
+import scipy.signal
+import soundfile as sf
 import matplotlib.pyplot as plt
 
-NFFT = 512
-PRE_EMPHASIS = 0.97
-FFT_LENGTH = 256
-HOP_LENGTH = 128
-FRAMES = 623
+WINDOW_OFFSET = np.finfo(np.float64).tiny
+FRAME_SIZE = 1024
+HOP_SIZE = 512
+QUARTER_FRAME = FRAME_SIZE // 4
 
-'''
-Load Data
-'''
+""" FFT test """
+# test data
 y, sr = sf.read("audio/mixed.wav")
 
-# plot
-plt.figure(figsize=(12, 5))
-plt.title("Original")
 plt.plot(y)
+plt.title("Original Data")
 plt.show()
 
-'''
-Pre-Emphasis
-'''
-emphasized_signal = np.append(y[0], y[1:] - PRE_EMPHASIS * y[:-1])
+# test numpy fft on audio
+f_fft = np.fft.rfft(y)
+y_fft = np.fft.irfft(f_fft)
+sf.write("audio_generated/mixed_fft.wav", y_fft, sr)
 
-# plot
-plt.figure(figsize=(12, 5))
-plt.title("Emphasized")
-plt.plot(emphasized_signal)
+''' My ISTFT '''
+
+# frame test
+frames = m.frame(y, FRAME_SIZE, HOP_SIZE)
+
+# foward STFT
+stft = m.stft_magnitude(y, FRAME_SIZE, HOP_SIZE, FRAME_SIZE)
+
+FRAMES = stft.shape[0]
+
+# Generate Window
+hann = m.periodic_hann(FRAME_SIZE)
+hann = hann + WINDOW_OFFSET
+
+# Preallocate output
+expected_signal_length = FRAME_SIZE + HOP_SIZE * (FRAMES - 1)
+y_out = np.zeros(expected_signal_length)
+
+for i in range(FRAMES):
+
+    # Inverse FFT (After checking produces negative value)
+    ifft = np.fft.irfft(stft[i, :]) 
+
+    # Add offset for dividing
+    ifft = ifft + WINDOW_OFFSET
+
+    # remove window
+    ifft = ifft / hann
+    
+    # Load into output
+    start = i * HOP_SIZE + QUARTER_FRAME
+
+    '''
+    title = "Frame %d" % i
+    fig, axs = plt.subplots(2)
+    fig.suptitle(title)
+    axs[0].plot(frames[i,:])
+    axs[1].plot(ifft[256 : 768])
+    plt.show()
+    '''
+
+    y_out[start : (start + HOP_SIZE)] = ifft[QUARTER_FRAME : QUARTER_FRAME * 3]
+
+plt.plot(y_out)
 plt.show()
 
-'''
-Framing
-'''
+sf.write("audio_generated/mixed_istft.wav", y_out, sr)
+sf.write("audio_generate/fart.wav", y_out - y, sr)
